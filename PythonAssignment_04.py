1206f1dbc3a10e72d2f11b5b6016680640acf656
Rohit <rohitrk.singh1920@gmail.com>
2025-11-22 20:50:46 +0530

    Add files via upload

---
+++ PythonAssignment_04.py
@@ -0,0 +1,32 @@
+# Examples Speed & Vectorization
+# import numpy as np
+
+# arr = np.array([1, 2, 3])
+# print(arr * 2)
+
+
+# Example Multidimensional Arrays
+# import numpy as np
+
+# mat = np.array([[1, 2], [3, 4]])
+# print(mat)
+
+
+# Create one-dimensional and two-dimensional arrays
+import numpy as np
+
+arr1 = np.array([10, 20, 30, 40, 50])
+
+arr2 = np.array([[1, 2, 3],[4, 5, 6]])
+
+print("----- One-Dimensional Array -----")
+print("Array:", arr1)
+print("Shape:", arr1.shape)
+print("Dimensions:", arr1.ndim)
+print("Data Type:", arr1.dtype)
+
+print("----- Two-Dimensional Array -----")
+print("Array:", arr2)
+print("Shape:", arr2.shape)
+print("Dimensions:", arr2.ndim)
+print("Data Type:", arr2.dtype)
